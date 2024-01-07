327ad5bc2db931163fb479072d0a1c01f3994880
Sadakat Hossain <sadakat.hossain11@gmail.com>
2024-01-07 11:56:39 +0700

    Change the design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-100 bg-gradient-to-b from-gray-800 to-black text-white about"
+      className="w-full text-center h-100 bg-gradient-to-b from-gray-800 to-black text-white about"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
